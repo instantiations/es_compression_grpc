12ad73440d929d8c972aa2e7ba54c335344025e1
Seth Berman <sberman@instantiations.com>
2021-04-05 18:05:44 -0400

    updated helloworld test to run pub get

--- test/helloworld_test.dart
+++ test/helloworld_test.dart
@@ -4,6 +4,7 @@
 
 import 'dart:io';
 
+import 'package:file_utils/file_utils.dart';
 import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 import 'package:test_process/test_process.dart';
@@ -13,13 +14,21 @@ void main() {
     TestProcess? server;
     TestProcess? client;
 
+    final origPath = path.current;
+    final helloWorldPath = path.join(path.current, 'example', 'helloworld');
     final serverPath =
-        path.join(path.current, 'example', 'helloworld', 'bin', 'server.dart');
+        path.join(helloWorldPath, 'bin', 'server.dart');
     final clientPath =
-        path.join(path.current, 'example', 'helloworld', 'bin', 'client.dart');
+        path.join(helloWorldPath, 'bin', 'client.dart');
 
     expect(File(serverPath).existsSync(), true);
     expect(File(clientPath).existsSync(), true);
+
+    // Run pub get on helloworld example
+    expect(FileUtils.chdir(helloWorldPath), true);
+    final pubGet = await TestProcess.start('pub', ['get']);
+    await pubGet.shouldExit(0);
+    expect(FileUtils.chdir(origPath), true);
 
     try {
       server = await TestProcess.start('dart', [serverPath]);
