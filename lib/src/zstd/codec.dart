fa12efc7ab24ae23b48675032ae6eab9c086996a
Seth Berman <sberman@instantiations.com>
2021-04-05 15:48:22 -0400

    Cleanup, adding tests

--- lib/src/zstd/codec.dart
+++ lib/src/zstd/codec.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2021, Instantiations, Inc. Please see the AUTHORS
+// file for details. All rights reserved. Use of this source code is governed by
+// a BSD-style license that can be found in the LICENSE file.
+
 import 'package:es_compression/zstd.dart';
 import 'package:grpc/grpc.dart' as grpc;
 
---
+++ test/codec_tests.dart
@@ -0,0 +1,33 @@
+// Copyright (c) 2021, Instantiations, Inc. Please see the AUTHORS
+// file for details. All rights reserved. Use of this source code is governed by
+// a BSD-style license that can be found in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:es_compression_grpc/codec.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('Test Brotli Codec encoding name', () {
+    const codec = BrotliCodec();
+    expect(codec.encodingName, 'brotli');
+  });
+
+  test('Test Lz4 Codec encoding name', () {
+    const codec = Lz4Codec();
+    expect(codec.encodingName, 'lz4');
+  });
+
+  test('Test Zstd Codec encoding name', () {
+    const codec = ZstdCodec();
+    expect(codec.encodingName, 'zstd');
+  });
+
+  test('Test Codec Compress/Decompress', () {
+    const codecs = [BrotliCodec(), Lz4Codec(), ZstdCodec()];
+    for(final codec in codecs) {
+      final testData = utf8.encode('Hello Dart!');
+      expect(codec.decompress(codec.compress(testData)), testData);
+    }
+  });
+}
